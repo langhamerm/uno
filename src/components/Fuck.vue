0f76b5b01a1658242b8680c575be941e81f69eda
Mark Langhamer <langhamerm@gmail.com>
2021-03-12 12:43:22 -0600

    firt compo

---
+++ src/components/Fuck.vue
@@ -0,0 +1,40 @@
+<template>
+	<div class="wtf">
+		<h1>{{ msg }}</h1>
+		<img :src="img" alt="pic" />
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Fuck",
+	props: {
+		msg: String,
+		img: String,
+	},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+	margin: 40px 0 0;
+}
+ul {
+	list-style-type: none;
+	padding: 0;
+}
+li {
+	display: inline-block;
+	margin: 0 10px;
+}
+a {
+	color: #42b983;
+}
+#wtf {
+background-size: cover;
+background-position: center;
+height: 500;
+width: 500;
+}
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="about">
+		<Fuck msg='WTF' img='https://i.pinimg.com/originals/37/78/ae/3778aef45dbcae3ed985c7ce2f7b6106.jpg' />
+	</div>
+</template>
+
+<script>
+import Fuck from "@/components/Fuck";
+export default {
+	name: "About",
+	components: {
+		Fuck,
+	},
+};
+</script>
